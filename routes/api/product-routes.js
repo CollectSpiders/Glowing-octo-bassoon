3c1da07c52b9d4b3b136a07d6e8f3c3c6af42bb8
CollectSpiders <mathewj519@gmail.com>
2023-07-19 19:04:56 -0700

    finished get 1 product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    // Include Category and Tag models in the query result
+    include: [Category, Tag],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    // Find the product with the given id
+    where: { id:req.params.id },
+    // Include Category and Tag models in the query result
+    include: [Category, Tag],
+  })
+    .then((product) =>{
+      if (!product){
+        // If the product is not found, return a 404 error
+        res.status(404).json({ message: 'No product found with this id' });
+        return;
+      }
+      res.json(product);
+    })
+    .catch((err) =>{
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
